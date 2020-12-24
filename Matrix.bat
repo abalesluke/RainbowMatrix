7d74db16c7750e6f5dd564ca8ec87c056262677b
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2020-12-24 11:31:26 +0800

    Create Matrix.bat

---
+++ Matrix.bat
@@ -0,0 +1,28 @@
+@echo off
+:random
+color 01
+
+echo %random% %random% %random% %random%
+%random% %random% %random%
+
+color 02
+
+echo %random% %random% %random% %random%
+%random% %random% %random%
+
+color 03
+
+echo %random% %random% %random% %random%
+%random% %random% %random%
+
+color 04
+
+echo %random% %random% %random% %random%
+%random% %random% %random%
+
+color 05
+
+echo %random% %random% %random% %random%
+%random% %random% %random%
+
+goto random
